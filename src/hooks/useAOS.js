09eb1ba52c96f9776881a59d52ee1fcb1159ca26
Sonhoccode <magonsilver05@gmail.com>
2026-02-12 02:39:25 +0700

    Optimize: Disable AOS animations on mobile for faster perceived load

--- src/hooks/useAOS.js
+++ src/hooks/useAOS.js
@@ -6,7 +6,7 @@ const useAOS = () => {
     useEffect(() => {
         AOS.init({
             // Global settings:
-            disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+            disable: 'mobile', // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
             startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
             initClassName: 'aos-init', // class applied after initialization
             animatedClassName: 'aos-animate', // class applied on animation
